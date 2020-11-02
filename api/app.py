13298dd0f4b0d6fa27d755aa1f59c7364cd73756
Takumi Kanada <candy12t@gmail.com>
2020-11-02 18:49:44 +0900

    Update: chat bot (#2)
    ランダムに文字(数字)を表示

--- api/app.py
+++ api/app.py
@@ -1,13 +1,23 @@
-from flask import Flask, request, jsonify
+import random
+
+from flask import Flask, jsonify
+
+MESSAGE = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False  # json形式をascii以外で扱う
 
 
+def get_seed():
+    seed = random.randrange(10)
+    return seed
+
+
 @app.route("/", methods=["POST"])
 def text_reaction():
-    request_json = request.get_json()
-    return jsonify({"message": request_json["text"]}), 200
+    seed = get_seed()
+    message = MESSAGE[seed]
+    return jsonify({"message": message}), 200
 
 
 if __name__ == "__main__":
