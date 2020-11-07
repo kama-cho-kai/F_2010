b0e6daf075495675b3014de37170600b64679856
Takumi Kanada <candy12t@gmail.com>
2020-11-07 10:39:38 +0900

    Add: todoist task, belogings (#15)

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 from flask import Flask, request, jsonify, redirect
 
 from positive_or_negative import predict
+from todoist_api import get_today_tasks, get_today_belogings
 
 
 app = Flask(__name__)
@@ -14,14 +15,11 @@ app.config["JSON_AS_ASCII"] = False  # json形式をascii以外で扱う
 load_dotenv()
 
 
-POSITIVE_MESSAGE = [0, 1, 2, 3, 4]
-NEGATIVE_MESSAGE = [5, 6, 7, 8, 9]
-
 TODOIST_AUTHORIZE_URL = 'https://todoist.com/oauth/authorize'
 TODOIST_ACCESS_TOKEN_URL = 'https://todoist.com/oauth/access_token'
 CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
-SCOPE = 'data:read'
+SCOPE = 'data:read_write'
 STATE = os.getenv('STATE')
 
 
@@ -30,19 +28,56 @@ def get_seed():
     return seed
 
 
-@app.route("/", methods=["POST"])
+@app.route("/get_message", methods=["POST"])
 def text_reaction():
     request_json = request.get_json()
     input_text = request_json['text']
-    seed = get_seed()
+    name = request_json['name']
+    token = request_json['token']
 
+    POSITIVE_MESSAGE = [
+           f"そうだったの？ {name}が元気だと僕も嬉しい！",
+           f"やったね〜！ {name}の話もっと聞かせて欲しいな",
+           "そうなの？ 最高な一日だね！",
+           f"{name}が楽しそうで僕も楽しい気分になったよ",
+           "良かったね！ きっと良いことは続くよ〜"
+            ]
+    NEGATIVE_MESSAGE = [
+            "そっかそっか...。 大変だったね",
+            "頑張ってね。 無理はしないでね！",
+            f"つらかったね。僕はいつでも{name}の味方だよ",
+            "そうだったんだね。もっとお話聞くよ?",
+            "そうだったんだね。もっとお話聞くよ?",
+            ]
+
+    seed = get_seed()
     if predict(input_text) == 'positive':
         message = POSITIVE_MESSAGE[seed]
+        task = get_today_tasks(token)
+        if len(task) != 0:
+            message += f" ところで、今日{task[0]}があるみたい。頑張ってね〜"
     else:
         message = NEGATIVE_MESSAGE[seed]
-    return jsonify({"message": message}), 200
+    return jsonify({"status": "success", "message": message}), 200
 
 
+@app.route("/get_tasks", methods=["POST"])
+def get_tasks():
+    request_json = request.get_json()
+    token = request_json['token']
+    tasks = get_today_tasks(token)
+    return jsonify({"status": "success", "tasks": tasks}), 200
+
+
+@app.route("/get_belogings", methods=["POST"])
+def get_belogings():
+    request_json = request.get_json()
+    token = request_json['token']
+    beloging = get_today_belogings(token)
+    return jsonify({"status": "success", "belogings": beloging}), 200
+
+
+# Todoist OAuth
 @app.route("/cooperation_todoist")
 def cooperation_todoist():
     url = f"{TODOIST_AUTHORIZE_URL}?client_id={CLIENT_ID}&scope={SCOPE}&state={STATE}"
